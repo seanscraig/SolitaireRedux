e0320abf0bb0e6b27bddfc2e0c110d62934c732f
Sean Craig <seancraig21@gmail.com>
2021-01-17 18:01:06 -0600

    Added Basic Card Interactivity
    Add Clicking on Deck Functionality

--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
+    private SolitaireGame solitaireGame;
     // Start is called before the first frame update
     void Start()
     {
-        
+        solitaireGame = FindObjectOfType<SolitaireGame>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetMouseClick();
+    }
+
+    void GetMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10));
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                // What has been hit? Deck/Card/Empty Slot
+                if (hit.collider.CompareTag("Deck"))
+                {
+                    // clicked deck
+                    Deck();
+                }
+                else if (hit.collider.CompareTag("Card"))
+                {
+                    // clicked card
+                    Card();
+                }
+                else if (hit.collider.CompareTag("Top"))
+                {
+                    // clicked top
+                    Top();
+                }
+                else if (hit.collider.CompareTag("Bottom"))
+                {
+                    // clicked bottom
+                    Bottom();
+                }
+            }
+        }
+    }
+
+    void Deck()
+    {
+        // deck click actions
+        //Debug.Log("Clicked on the Deck");
+        solitaireGame.DealFromDeck();
+    }
+
+    void Card()
+    {
+        // card click actions
+        Debug.Log("Clicked on a Card");
+    }
+
+    void Top()
+    {
+        // top click actions
+        Debug.Log("Clicked on a Top Slot");
+    }
+
+    void Bottom()
+    {
+        // bottom click actions
+        Debug.Log("Clicked on a Bottom Slot");
     }
 }
